d7885d99f77112d9c8ca45da519650b237a74bf9
natalia-siladi <80765980+natalia-siladi@users.noreply.github.com>
2022-08-13 03:43:40 +0000

    Deploying to gh-pages from @ natalia-siladi/goit-js-hw-09@5448a07658aef774c5cd85e48e299a4cc3de9233 🚀

---
+++ 01-color-switcher.f2948cfb.js
@@ -0,0 +1,2 @@
+let t=null;const e={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};e.startBtn.addEventListener("click",(function(){t=setInterval((()=>e.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)})),e.stopBtn.addEventListener("click",(function(){clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.f2948cfb.js.map
