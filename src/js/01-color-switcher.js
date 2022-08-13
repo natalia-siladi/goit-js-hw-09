5448a07658aef774c5cd85e48e299a4cc3de9233
Natalia-siladi <vladsil2k19@gmail.com>
2022-08-13 06:43:10 +0300

    добавляет задание 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+let intervalId = null;
+
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function onStartBtnClick() {
+    intervalId = setInterval(() => refs.body.style.backgroundColor = getRandomHexColor(), 1000);
+
+}
+
+function onStopBtnClick() {
+    clearInterval(intervalId);
+};
 
