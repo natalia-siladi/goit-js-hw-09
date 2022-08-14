4069d0524da4426f35f34db529c12873f94a23dc
Natalia-siladi <vladsil2k19@gmail.com>
2022-08-14 23:18:02 +0300

    добавляет правки

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,23 +1,32 @@
-let intervalId = null;
+
 
 const refs = {
     body: document.querySelector('body'),
     startBtn: document.querySelector('button[data-start]'),
     stopBtn: document.querySelector('button[data-stop]'),
 }
+let intervalId = null;
+refs.stopBtn.disabled = true;
 
 refs.startBtn.addEventListener('click', onStartBtnClick);
 refs.stopBtn.addEventListener('click', onStopBtnClick);
 
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+
 function onStartBtnClick() {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
     intervalId = setInterval(() => refs.body.style.backgroundColor = getRandomHexColor(), 1000);
 
-}
+};
 
 function onStopBtnClick() {
     clearInterval(intervalId);
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+
+
 };
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
